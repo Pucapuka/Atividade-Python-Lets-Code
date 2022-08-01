57b9c0ab736ebd32adfffb9dc61b13f7e1b1cf06
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2022-08-01 19:11:50 -0300

    Create Tuplas1.py

---
+++ Tuplas1.py
@@ -0,0 +1,13 @@
+#Crie uma tupla com todos os números de 0 a 9
+
+#Tupla com parênteses
+print("Essa é uma tupla com parênteses:")
+ex_tupla_1 = ('0','1','2','3','4','5','6','7','8','9')
+print(ex_tupla_1)
+print(f"essa coleção é de {type(ex_tupla_1)}")
+
+#Tupla sem parênteses (implíscita)
+print("Essa é uma tupla sem parênteses")
+ex_tupla_2 = '0','1','2','3','4','5','6','7','8','9'
+print(ex_tupla_1)
+print(f"essa coleção é de {type(ex_tupla_2)}")
