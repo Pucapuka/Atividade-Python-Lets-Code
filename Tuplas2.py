bffa78a959deb78b395e0df8d2d1f51cf5202e4c
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2022-08-01 19:37:03 -0300

    Create Tuplas2.py

---
+++ Tuplas2.py
@@ -0,0 +1,6 @@
+#Crie uma tupla com todos os pares entre 0 e 100 (inclusive)
+
+
+tupla = tuple (range(0,101,2)) #criando variável 'tupla' tipo 'tuple' com range (note que coloquei '101' para incluir o '100'
+print (tupla)
+print (type(tupla))  #checando se é uma tupla
